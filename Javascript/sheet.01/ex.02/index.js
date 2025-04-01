d3e20e4bfaa92af8328cf956af94ab159811d1e4
ulippuden99 <146721319+ulippuden99@users.noreply.github.com>
2025-04-01 13:41:13 +0200

    Ultimo cambio al ejercicio

--- Javascript/sheet.01/ex.02/index.js
+++ Javascript/sheet.01/ex.02/index.js
@@ -1,7 +1,8 @@
-let numero=42
+let numero=42 // Lo que hacemos es declarar la variable con let y NombreVariable y la igualamos al tipo de dato
 let string="Hola string"
 let booleano=true
 
+// Si lo cambiamos en todos pues nos imprimirá por pantalla sus respectivos tipos de datos:
 console.log(numero); // Esto solo imprimirá el número 42
 console.log(typeof numero); // Esto imprimirá "number", el tipo de dato de 42
 
