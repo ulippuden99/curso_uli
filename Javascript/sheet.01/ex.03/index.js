b2ff9130a42dc1c0102ecc850852a5522002cb67
ulippuden99 <146721319+ulippuden99@users.noreply.github.com>
2025-04-01 13:48:59 +0200

    Cambio incompleto

--- Javascript/sheet.01/ex.03/index.js
+++ Javascript/sheet.01/ex.03/index.js
@@ -1,5 +1,9 @@
-let a=10;
-let b=5;
+let a=10; // Declaramos la variable a
+let b=5; // Declaramos la variable b
 
+ console.log(`${a} + ${b}`); //Ahora imprimimos por pantalla el resultado de la suma entre a y b
+ console.log(`${a} + ${b}`); //Ahora imprimimos por pantalla el resultado de la resta entre a y b
+ console.log(`${a} + ${b}`); //Ahora imprimimos por pantalla el resultado de la multiplicacion entre a y b
+ console.log(`${a} + ${b}`); //Ahora imprimimos por pantalla el resultado de la division entre a y b
 
-
+//FALTA ACABAR MUCHAS COSAS Y ESTA MAL
