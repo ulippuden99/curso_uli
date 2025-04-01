15e7f21d7db1c4a2cf9a37834e98a01b6cabef9f
ulippuden99 <146721319+ulippuden99@users.noreply.github.com>
2025-04-01 13:25:05 +0200

    Carpeta nueva con ejercicios añadidos

---
+++ Javascript/sheet.01/ex.01/index.js
@@ -0,0 +1,1 @@
+console.log("Hola, Mundo!"); //Imprime un "Hola, Mundo! con el console.log"
---
+++ Javascript/sheet.01/ex.02/index.js
@@ -0,0 +1,9 @@
+console.log(42); // Esto solo imprimirá el número 42
+console.log(typeof 42); // Esto imprimirá "number", el tipo de dato de 42
+
+console.log("Hola string"); // Imprime el texto "Hola string"
+console.log(typeof "Hola string"); // Imprime "string", el tipo de dato de la cadena
+
+console.log(true); // Imprime el valor booleano true
+console.log(typeof true); // Imprime "boolean", el tipo de dato de true
+
