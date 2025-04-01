9766e783d2cb49b239feaea93d7ed75519df9026
ulippuden99 <tomibonannuli@gmail.com>
2025-04-01 18:42:09 +0200

    Ejercicio actualizado correctamente

--- Javascript/sheet.01/ex.03/index.js
+++ Javascript/sheet.01/ex.03/index.js
@@ -1,9 +1,7 @@
 let a=10; // Declaramos la variable a
 let b=5; // Declaramos la variable b
 
- console.log(`${a} + ${b}`); //Ahora imprimimos por pantalla el resultado de la suma entre a y b
- console.log(`${a} + ${b}`); //Ahora imprimimos por pantalla el resultado de la resta entre a y b
- console.log(`${a} + ${b}`); //Ahora imprimimos por pantalla el resultado de la multiplicacion entre a y b
- console.log(`${a} + ${b}`); //Ahora imprimimos por pantalla el resultado de la division entre a y b
-
-//FALTA ACABAR MUCHAS COSAS Y ESTA MAL
+ console.log(`El resultado de la suma es: ${a+b}`); //Ahora imprimimos por pantalla el resultado de la suma entre a y b
+ console.log(`El resultado de la resta es:${a-b}`); //Ahora imprimimos por pantalla el resultado de la resta entre a y b
+ console.log(`El resultado de la multiplicacion es: ${a*b}`); //Ahora imprimimos por pantalla el resultado de la multiplicacion entre a y b
+ console.log(`El resultado de la division es:${a/b}`); //Ahora imprimimos por pantalla el resultado de la division entre a y b
