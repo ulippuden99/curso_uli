6bf026e097435ff3b5259d1cad81ad5da99d7d08
ulippuden99 <146721319+ulippuden99@users.noreply.github.com>
2025-04-03 09:46:06 +0200

    Ejercicio sctuslizado

--- Javascript/sheet.01/ex.05/index.js
+++ Javascript/sheet.01/ex.05/index.js
@@ -1,1 +1,13 @@
-let i = 1
+let i = 1; // Declaramos la variable
+console.log("Esto es un bucle for") // Imprimimos esto para saber que bucle se esta ejecutando
+for (i; i <= 10; i++) {  // Colocamos el bucle for con la sintaxis (variable,condicion,incremento)
+    console.log(i) // Mostramos el valor de i
+}
+
+console.log("Esto es un bucle while") // Imprimimos esto para saber que bucle se esta ejecutando
+let a = 0; // Declaramos la variable
+while (a < 10) { // Colocamos la condicion
+    a++ // Aplicamos el incremento
+    console.log(a) // Mostramos el valor de a
+}
+
