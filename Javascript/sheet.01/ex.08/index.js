4cba11422ebca76cbd1dd09cf92f55ec3be68b41
ulippuden99 <146721319+ulippuden99@users.noreply.github.com>
2025-04-04 13:56:22 +0200

    Ejercicio incompleto

---
+++ Javascript/sheet.01/ex.08/index.js
@@ -0,0 +1,12 @@
+let persona = {name: "Ulises", age: 22, ciudad: "Malaga"}; // Declaramos el objeto persona y dentro de los corchetes colocsmod lsd propiedades
+                                            
+console.log(persona.name) // Imprimimos por pantalla  objeto.propiedad que queremos mostrar, en este caso name
+console.log(persona.age) // Imprimimos por pantalla  objeto.propiedad que queremos mostrar, en este caso age
+console.log(persona.ciudad) // Imprimimos por pantalla  objeto.propiedad que queremos mostrar, en este caso ciudad
+
+
+ function describirPersona(persona)  {
+    let persona = {name: "Ulises", age: 22, ciudad: "Malaga"};  
+ }
+
+ return(describirPersona)
