4c3b631b0230840e524e72f7a1b4bdb760f14748
ulippuden99 <tomibonannuli@gmail.com>
2025-04-07 18:58:03 +0200

    Ejercicio completado

--- Javascript/sheet.02/ex.03/index.js
+++ Javascript/sheet.02/ex.03/index.js
@@ -1,7 +1,9 @@
-const palabras = ["hola", "mundo"];
+const palabras = ["hola", "mundo"]; // Declaramos el array co []
 
-function convertirMayusculas(palabras) {
-    palabras.map()
-    return palabras
+function convertirMayusculas(palabras) { // Declaramos la funcion 
+    const resultado = palabras.map(nombre => nombre.toUpperCase()) //Declaramos la n ueva varable  
+                                    //la igualamos a la antigua variable con el .map para cambiar las palabras a Mayusculas
+    return resultado;  // Que nos devuelva el resultado
 }
-console.log(convertirMayusculas(palabras));
+console.log(convertirMayusculas(palabras)); // Que se vea por pantalla el cambio
+
