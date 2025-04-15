da42add8a11dbcb26db600264290b73ce9ed7a73
ulippuden99 <146721319+ulippuden99@users.noreply.github.com>
2025-04-15 09:37:18 +0200

    Ejercicio añadido

--- Javascript/sheet.02/ex.04/index.js
+++ Javascript/sheet.02/ex.04/index.js
@@ -1,4 +1,4 @@
-const numeros = [1, 2, 3, 4, 5];  // Declaramos el array
+const numeros = [1, 2, 3, 4, 5];  // Declaramos el objeto
 
 function sumarNumeros(numeros) { // Declaramos la funcion 
     const resultado = numeros.reduce((acumulador, numeros) =>{return acumulador + numeros;},0) //Declaramos la nueva variable la igualamos a la variable con .reduce
